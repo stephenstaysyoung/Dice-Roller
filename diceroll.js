934fffb894090c4986891909a72a9fba547bc052
Stephen <stephenstaysyoung@gmail.com>
2017-03-01 19:01:54 -0700

    broke d6 function into if-statement
    Added if statement for the first click or all others. The initial d6
    roll will generate a <p> element and display the result. Other clicks
    will replace the roll with a new result. Issues with applying an ID to
    the <p> created and replacing it.

--- diceroll.js
+++ diceroll.js
@@ -5,6 +5,7 @@ var dieRoll4,
     dieRoll12,
     dieRoll20,
     dieRoll100,
+    d6Click,
     d6;
 
 
@@ -12,15 +13,24 @@ function getDieRoll4 () {
   dieRoll4 = Math.floor(Math.random() * 4)+1;
 }
 
+
 var d6 = document.getElementById('d6');
+var d6Click = 0;
+
 d6.onclick = function () {getDieRoll6()};
+
 function getDieRoll6 () {
   dieRoll6 = Math.floor(Math.random() * 6)+1;
-  var p6 = document.createElement("p");
-  var d6Result = document.createTextNode(dieRoll6)
-  p6.appendChild(d6Result);
-    document.body.appendChild(p6);
-}
+    if (d6Click >= 1) {
+      document.getElementById("pd6").innerHTML = dieRoll6;
+    } else {
+      var p6 = document.createElement("p").setAttribute("id", "pd6");
+        var d6Result = document.createTextNode(dieRoll6)
+        p6.appendChild(d6Result);
+        d6Click++;
+          }
+        }
+
 
 function getDieRoll8 () {
   dieRoll8 = Math.floor(Math.random() * 8)+1;
